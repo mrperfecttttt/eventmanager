99ccd8275919cf2bf0be78d24f07d60034d16751
mrperfecttttt <wanmuhammadfikri@graduate.utm.my>
2024-10-13 22:58:32 +0800

    latest

--- db_connect.php
+++ db_connect.php
@@ -1,1 +1,14 @@
-<?php$servername = "209.97.172.153"; $username = "admin"; $password = "P@55w0rd"; $database = "tunang"; // Create connection$conn = new mysqli($servername, $username, $password, $database);// Check connectionif ($conn->connect_error) {    die("Connection failed: " . $conn->connect_error);}?>
+<?php
+$servername = "209.97.172.153"; 
+$username = "admin"; 
+$password = "P@55w0rd"; 
+$database = "tunang"; 
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $database);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+?>
