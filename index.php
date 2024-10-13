918559cfcaccfc2e6e0f87548bab2827e7075a46
mrperfecttttt <wanmuhammadfikri@graduate.utm.my>
2024-10-14 00:50:47 +0800

    latest

--- index.php
+++ index.php
@@ -35,6 +35,10 @@ $totalPax = $totalPaxResult->fetch_assoc()['totalPax'];
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="apple-mobile-web-app-capable" content="yes">
+    <meta name="apple-mobile-web-app-status-bar-style" content="black">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+
     <title>RSVP Management</title>
     <link rel="stylesheet" href="style.css">
     <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap" rel="stylesheet">
