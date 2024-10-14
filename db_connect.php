e6fae3bfc4a55c54187be093b4a65f9011ee378f
mrperfecttttt <wanmuhammadfikri@graduate.utm.my>
2024-10-15 02:09:27 +0800

    latestjk

--- db_connect.php
+++ db_connect.php
@@ -1,8 +1,20 @@
 <?php
+// Start the session if it hasn't been started
+if (session_status() === PHP_SESSION_NONE) {
+    session_start();
+}
+
+// Database connection details
 $servername = "209.97.172.153"; 
 $username = "admin"; 
 $password = "P@55w0rd"; 
-$database = "creds"; 
+
+// Determine which database to use
+if (isset($_SESSION['db_name'])) {
+    $database = $_SESSION['db_name']; // Use the database name from the session
+} else {
+    $database = "creds"; // Use the default creds database
+}
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $database);
@@ -12,4 +24,3 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 ?>
-
