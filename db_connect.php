04e5443d51e0a8b54ef17aae577a669e82518f49
mrperfecttttt <wanmuhammadfikri@graduate.utm.my>
2024-10-15 00:47:01 +0800

    latest

--- db_connect.php
+++ db_connect.php
@@ -1,22 +1,10 @@
 <?php
-// session_start();  // Start session (if not already started)
-
-// Static connection values (server, username, and password remain the same)
 $servername = "209.97.172.153"; 
 $username = "admin"; 
-$password = "P@55w0rd";
-$database = "tunang";
+$password = "P@55w0rd"; 
+$database = "tunang"; 
 
-// Check if the user is logged in and if their specific database is set in the session
-if (isset($_SESSION['db_name'])) {
-    // If the session exists, use the user's specific database
-    $database = $_SESSION['db_name'];
-} else {
-    // If no session, use the default database
-    $database = "credentials";  // The default database for authentication and general data
-}
-
-// Create connection to the determined database
+// Create connection
 $conn = new mysqli($servername, $username, $password, $database);
 
 // Check connection
